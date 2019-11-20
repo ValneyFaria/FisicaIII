4904b7d55b2f93a6f72b20187f115a10ccc8204d
valneyfaria <valneyfaria@hotmail.com>
2019-11-20 09:30:51 -0300

    Mudança nos nomes

---
+++ ex_2_a.py
@@ -0,0 +1,30 @@
+# !/usr/bin/python3
+# coding: UTF-8
+
+"""
+	Um elétron é projetado com uma velocidade inicial Vi = 3 * 10^6 m/s na direção x na região entre duas placas carregadas com cargas opostas. Quando o elétron deixa a região entre as placas, ele sofreu uma deflexão vertical de 2cm. Suponha que o campo elétrico entre as placas e que o campo elétrico fora da região das placas seja nulo.
+
+	a) Qual a intensidade do campo elétrico entre as placas?
+	b) Em qual ponto Yf sobre a tela localizada a 1m de distância das placas o elétron irá colidir?
+"""
+
+# F = q*E = m*a
+# E = (me/qe)*a
+
+"""
+	comp_placa = 0.5
+	distancia_tela = 1.0
+
+	vi = 3*10**6
+	x0 = 0
+	v0x = vi
+
+	x = x0 + v0x*t + 1/2*ax*t**2
+	x = vi*t
+
+	t = x/vi = 0.5 / (3*10^6)
+	t = 0.166 * 10**(-6) s
+
+	y = y0 + v0y*t + (1/2)*ay*t**2
+	y = (1/2)*a*t**2 => a = 2y/t**2 => NÃO TERMNEI AQUI
+"""
